36425914ffa7eaaea39d22876eb716fa6d579080
James Edwards <edwards.econn@gmail.com>
2022-05-29 18:09:26 -0400

    Created header, carousel, and footer components

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // The ReactDOM.render method is used to render a react element into the actual DOM
 // The first argument is the component we want to render, and the second is the container element on the page
