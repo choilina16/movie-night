8ed4fe34aefed790e1aa6b2f8b810aee2c4fc074
Lina Choi <choilina16@gmail.com>
2022-05-29 23:39:55 -0400

    editing logo

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './header.css';
+import image from '../../assets/mao-png.png';
 
 function Header() {
-    return (
-        <header>
-            <img class="logo" alt="logo"></img>
-            <h1>MOVIE<span>NIGHT</span></h1>
-        </header> 
-    );
+  return (
+    <header>
+      <img src={image} class="logo" alt="logo"></img>
+      <h1>
+        MOVIE<span>NIGHT</span>
+      </h1>
+    </header>
+  );
 }
-
 
 export default Header;
