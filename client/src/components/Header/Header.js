3031c1a63c359ab8ab0a6e06dfe85d98d4180200
Lina Choi <choilina16@gmail.com>
2022-05-30 14:29:12 -0400

    updating inputbox

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import image from '../../assets/mao-png.png';
 function Header() {
   return (
     <header>
-      <img src={image} class="logo" alt="logo"></img>
+      {/* <img src={image} class="logo" alt="logo"></img> */}
       <h1>
         MOVIE<span>NIGHT</span>
       </h1>
--- client/src/components/InputBox/InputBox.js
+++ client/src/components/InputBox/InputBox.js
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
+import { alpha, styled } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+// import Box from '@mui/material/Box';
 import Autocomplete from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
 import { makeStyles } from '@mui/styles';
@@ -35,20 +40,65 @@ const movieLanguage = [
   { label: 'French', id: 6 },
 ];
 
+// mui styling
 const useStyles = makeStyles({
   root: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    border: 'red',
   },
 });
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  'label + &': {
+    marginTop: theme.spacing(3),
+  },
+  '& .MuiInputBase-input': {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    width: 'auto',
+    padding: '10px 12px',
+    transition: theme.transitions.create([
+      'border-color',
+      'background-color',
+      'box-shadow',
+    ]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}));
 
 function InputBox() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <div className="username-container">
-        <TextField id="outlined-basic" label="Usernames" variant="outlined" />
+        {/* <TextField id="outlined-basic" label="Usernames" variant="outlined" /> */}
+        <FormControl variant="standard">
+          <InputLabel shrink htmlFor="bootstrap-input">
+            Bootstrap
+          </InputLabel>
+          <BootstrapInput defaultValue="react-bootstrap" id="bootstrap-input" />
+        </FormControl>
         <Button variant="contained" size="small">
           ADD
         </Button>
