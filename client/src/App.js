fc6af1e9f8b22e933a4542de341e3e90b00e3ffa
James Edwards <edwards.econn@gmail.com>
2022-05-31 17:48:22 -0400

    Will fix MUI in media query, but first added 4 cards for movies

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import AppCarousel from './components/Carousel/Carousel';
 // import InputBox from './components/InputBox/InputBox';
-import PostSubmit from './pages/PostSubmit';
+import PostSubmit from './pages/PostSubmit/PostSubmit';
+import * as ReactBootStrap from "react-bootstrap";
 
 
 function App() {
