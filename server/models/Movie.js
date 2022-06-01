491d6b4d1005ae0968c4d3158bc113383907846e
AnthonyC233 <changanthony61@yahoo.com>
2022-06-01 12:10:12 -0400

    Models folder update

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,1 +1,62 @@
-// module.exports = Movie;
+const { Schema, model } = require('mongoose')
+
+const matchupSchema = new Schema({
+  movies: [
+    {
+      type: String,
+    },
+  ],
+    movie_url: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    watchlist: {
+      type: Boolean,
+      required: true
+    },
+    film_id: {
+      type: String,
+      required: true
+    },
+    year: {
+      type: String,
+      required: true
+    },
+    cast: {
+      type: String,
+      required: true
+    },
+    genres: {
+      type: String,
+      required: true
+    },
+    language: {
+      type: String,
+      required: true
+    },
+    rating: {
+      type: String,
+      required: true
+    },
+    synopsis: {
+      type: String,
+      required: true
+    },
+    director: {
+      type: String,
+      required: true
+    },
+    runtime: {
+      type: String,
+      required: true
+    },
+});
+  
+
+
+
+module.exports = Movie(matchupSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,23 @@
-// module.exports = User;
+const { schema, model } = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const movieSchema = require('./Movie')
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      // set savedMovies to be an array of data that adheres to the movieSchema
+      savedMovies: [movieSchema],
+    },
+    // set this to use virtual below
+    {
+      toJSON: {
+        virtuals: true,
+      },
+    }
+  );
+module.exports = User(userSchema);
