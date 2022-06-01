f08060d76056b3a58d708bf4a03eb2eb80b506f1
Noah Brunner <noahneville@gmail.com>
2022-05-31 23:42:13 -0400

    adding logic to get data for individual movies

--- server/data-scraping/webScrape.js
+++ server/data-scraping/webScrape.js
@@ -23,17 +23,43 @@ async function scrapeData() {
     //console.log(data);
     const $ = cheerio.load(data);
     const filmList = $(".film-poster");
-    // Stores data for all films
-    const films = [];
+    // Stores data for all films in watchlist
+    const watchlist = [];
+    const movieModel = {};
     // Use .each method to loop through the li we selected
     filmList.each((idx, el) => {
      // Object holding data for each movie
       const movie = $(el , 'data-film-name').attr('data-film-slug');
       // Populate films  array with individual movie data
-      films.push(movie);
+      watchlist.push(movie);
     });
     // Logs countries array to the console
-    console.log(films);
+    console.log(watchlist);
+
+    for(let i = 0; i < watchlist.length; i++) {
+      console.log(watchlist[i]);
+
+      const singleMovieURL = "https://letterboxd.com" + watchlist[i];
+      console.log(singleMovieURL);
+      
+      const movieData =  await axios.get(singleMovieURL);
+      // console.log(movieData);
+
+      const $$ = cheerio.load(movieData);
+
+      const movieEntry = {
+        title: "",
+        year: ""
+      }
+
+      movieEntry.title = $$("h1 .headline-1");
+      // movieEntry.year = $$('h1 .headline-1', 'small .number').attr(a);
+
+      console.log(movieEntry);
+
+
+
+    }
     // Write countries array in countries.json file
     // fs.writeFile("films.json", JSON.stringify(films, null, 2), (err) => {
     //   if (err) {
