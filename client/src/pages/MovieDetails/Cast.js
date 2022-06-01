e5e0b25581e7f296b5a1be6be5febf749dad326a
Lina Choi <choilina16@gmail.com>
2022-06-01 11:07:20 -0400

    updating the movie details page so that it's easier to push in information later

--- client/src/pages/MovieDetails/Cast.js
+++ client/src/pages/MovieDetails/Cast.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 export default function Cast() {
+  const movieDetails = {
+    poster_url: '"https://i.mydramalist.com/d6jmK_4c.jpg?v=1',
+    title: 'Children of Troubled Times',
+    year: '1935',
+    rating: '5',
+    director: 'Xingzhi Xu',
+    cast: 'Mao, Mao, Mao, Miller',
+    genre: 'Drama',
+    language: 'Chinese',
+    synopsis:
+      'Arise! Arise! Arise! A heart throbbing tale of a young Mao Zedong, savior of China, and peruser of justice to the working class. None have graced this earth like Mao. Mao is the best I love Mao so much he is my hero and not your hero but he should be your hero thank you yes nihao ma. HAI.',
+  };
+
   return (
     <div>
-      <p>MAO, MAO, MAO, MILLER</p>
+      <p style={{ textTransform: 'uppercase' }}>{movieDetails.cast}</p>
     </div>
   );
 }
--- client/src/pages/MovieDetails/MovieDetails.js
+++ client/src/pages/MovieDetails/MovieDetails.js
@@ -10,6 +10,20 @@ import { BsFillStarFill } from 'react-icons/bs';
 
 // this is all placeholder information!
 export default function MovieDetails() {
+  // placeholder
+  const movieDetails = {
+    poster_url: '"https://i.mydramalist.com/d6jmK_4c.jpg?v=1',
+    title: 'Children of Troubled Times',
+    year: '1935',
+    rating: '5',
+    director: 'Xingzhi Xu',
+    cast: 'Mao, Mao, Mao, Miller',
+    genre: 'Drama',
+    language: 'Chinese',
+    synopsis:
+      'Arise! Arise! Arise! A heart throbbing tale of a young Mao Zedong, savior of China, and peruser of justice to the working class. None have graced this earth like Mao. Mao is the best I love Mao so much he is my hero and not your hero but he should be your hero thank you yes nihao ma. HAI.',
+  };
+
   const [currentPage, setCurrentPage] = useState('Cast');
 
   const renderPage = () => {
@@ -31,14 +45,14 @@ export default function MovieDetails() {
         <div className="movie-poster">
           <img src={movie1} alt="placeholder"></img>
           <p>
-            DIRECTOR: <span>XINGZHI XU</span>
+            DIRECTOR: <span>{movieDetails.director}</span>
           </p>
         </div>
 
         <div className="movie-details">
           <div>
-            <p className="movie-year">1935</p>
-            <h3>CHILDREN OF TROUBLED TIMES</h3>
+            <p className="movie-year">{movieDetails.year}</p>
+            <h3>{movieDetails.title}</h3>
             <p>‘风云儿女’</p>
             <hr></hr>
             <p className="stars">
@@ -60,13 +74,7 @@ export default function MovieDetails() {
       </div>
 
       <div className="movie-synopsis">
-        <p>
-          Arise! Arise! Arise! A heart throbbing tale of a young Mao Zedong,
-          savior of China, and persuer of justice to the working class. None
-          have graced this earth like Mao. Mao is the best I love Mao so much he
-          is my hero and not your hero but he should be your hero thank you yes
-          nihao ma. HAI.
-        </p>
+        <p>{movieDetails.synopsis}</p>
       </div>
     </div>
   );
