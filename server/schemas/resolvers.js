87618ea32b7066336e831f4c95d9c35d03ec8b0b
Noah Brunner <noahneville@gmail.com>
2022-06-02 14:08:45 -0400

    commiting cause the electrical grid in EAV is a nightmare

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,9 +9,13 @@ const resolvers = {
           },
     },
     Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user;
+        }
     },
 
-},
+};
 
 module.exports = resolvers;
 
