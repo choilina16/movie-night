0f5003b0a674c2017b0a9406cf2b73118774dff6
James Edwards <edwards.econn@gmail.com>
2022-06-03 01:14:06 -0400

    Flip cards work on hover

--- client/src/pages/PostSubmit/PostSubmit.js
+++ client/src/pages/PostSubmit/PostSubmit.js
@@ -1,100 +1,106 @@
 import React from 'react';
-import './postsubmit.css'
-import { Card } from "react-bootstrap";
+import FlipCard from './FlipCard';
 
 const PostSubmit = () => {
 
-    const cardInfo = [
+    const cards = [
       {
+        variant: "hover",
         image: "https://images-na.ssl-images-amazon.com/images/I/81wrceCbOWL._RI_.jpg",
         title: "The East is Red",
         text: "Exceptional movie",
       },
       {
+        variant: "hover",
         image:
           "https://m.media-amazon.com/images/M/MV5BOTI4NTNhZDMtMWNkZi00MTRmLWJmZDQtMmJkMGVmZTEzODlhXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_.jpg",
         title: "High and Low",
         text: "Great movie",
       },
       {
+        variant: "hover",
         image:
           "https://i.mydramalist.com/d6jmK_4c.jpg?v=1",
         title: "Children of Troubled Times",
         text: "Stupendous film",
       },
       {
+        variant: "hover",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Seven_Samurai_poster.jpg/1200px-Seven_Samurai_poster.jpg",
         title: "Michael Jordan",
         text: "Jawdropping picture",
       },
       {
+        variant: "hover",
         image: "https://images-na.ssl-images-amazon.com/images/I/81wrceCbOWL._RI_.jpg",
         title: "The East is Red",
         text: "Exceptional movie",
       },
       {
+        variant: "hover",
         image:
           "https://m.media-amazon.com/images/M/MV5BOTI4NTNhZDMtMWNkZi00MTRmLWJmZDQtMmJkMGVmZTEzODlhXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_.jpg",
         title: "High and Low",
         text: "Great movie",
       },
       {
+        variant: "hover",
         image:
           "https://i.mydramalist.com/d6jmK_4c.jpg?v=1",
         title: "Children of Troubled Times",
         text: "Stupendous film",
       },
       {
+        variant: "hover",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Seven_Samurai_poster.jpg/1200px-Seven_Samurai_poster.jpg",
         title: "Seven Samurai",
         text: "Jawdropping picture",
       },
       {
+        variant: "hover",
         image: "https://images-na.ssl-images-amazon.com/images/I/81wrceCbOWL._RI_.jpg",
         title: "The East is Red",
         text: "Exceptional movie",
       },
       {
+        variant: "hover",
         image:
           "https://m.media-amazon.com/images/M/MV5BOTI4NTNhZDMtMWNkZi00MTRmLWJmZDQtMmJkMGVmZTEzODlhXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_.jpg",
         title: "High and Low",
         text: "Great movie",
       },
       {
+        variant: "hover",
         image:
           "https://i.mydramalist.com/d6jmK_4c.jpg?v=1",
         title: "Children of Troubled Times",
         text: "Stupendous film",
       },
       {
+        variant: "hover",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Seven_Samurai_poster.jpg/1200px-Seven_Samurai_poster.jpg",
-        title: "Michael Jordan",
+        title: "Seven Samurai",
         text: "Jawdropping picture",
       },
     ];
   
-    // Flippable Cards
-    const renderCard = (card, index) => {
-      return (
-        <Card style={{ width: '18rem' }} key={index} className="box card-box col-lg-3">
-          <Card.Img variant="top" src={card.image} className="post-img"/>
-          <Card.Body>
-            <Card.Title className="card-title">{card.title}</Card.Title>
-            <Card.Text className="card-text">{card.text}</Card.Text>
-          </Card.Body>
-        </Card>
-      );
-    };
-  
-    return <div className="container">
-              <div className="row card-row">
-                  {cardInfo.map(renderCard)}
-              </div>
-           </div>;
+    // Render Cards by...
+    return (
+        // Container
+        <div className="container">
+            {/* Card Row */}
+            <div className="row card-row"> 
+            {/* ...mapping over each object in the `Cards` array of objects */}
+            {cards.map((card) => (
+                <FlipCard card={card} />
+            ))}
+            </div>
+        </div>
+    );
   };
   
+  // Exports to `App.js` to be rendered to page
   export default PostSubmit;
-  
