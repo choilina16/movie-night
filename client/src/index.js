9633b9f8364227a850636a6619399f6fdb5d0462
Lina Choi <choilina16@gmail.com>
2022-06-05 00:33:34 -0400

    query works

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./style.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 
 // The ReactDOM.render method is used to render a react element into the actual DOM
 // The first argument is the component we want to render, and the second is the container element on the page
-// Injecting whatever is in <App /> into the id of root
-ReactDOM.render(<App />, document.getElementById("root"));
+// Injecting whatever is in <App /> into the id of root\
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // React library handles grabbing the scripts
 
